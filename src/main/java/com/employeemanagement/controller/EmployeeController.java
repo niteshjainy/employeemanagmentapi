3d6ca3f462cb882bd30845d563c7b60c9dba4ddb
niteshji <nitesh@gmail.com>
2020-04-16 14:50:22 +0530

    new class file Employee

--- src/main/java/com/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/employeemanagement/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.employeemanagement.controller;
 
+import com.employeemanagement.dto.Employee;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -9,5 +14,13 @@ public class EmployeeController {
     public String insert()
     {
         return "Hello Sir";
+    }
+    @RequestMapping("employees")
+    public List<Employee> getAllEmployee(){
+        return (Arrays.asList(
+                new Employee(1, "Nitesh Jain", "TCS"),
+                new Employee(2, "Chetan Sul", "Accenture"),
+                new Employee(3, "Vijay Yadav", "")
+        ));
     }
 }
--- src/main/java/com/employeemanagement/dto/Employee.java
+++ src/main/java/com/employeemanagement/dto/Employee.java
@@ -6,6 +6,15 @@ private int empId;
 private String empName;
 private String deptName;
 
+    public Employee() {
+    }
+
+    public Employee(int empId, String empName, String deptName) {
+        this.empId = empId;
+        this.empName = empName;
+        this.deptName = deptName;
+    }
+
     public int getEmpId() {
         return empId;
     }
